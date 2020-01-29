a0a6b1a3b63354f7cf8a3e4918a829ae0ef47e54
gaellafond <g.lafond@aims.gov.au>
2020-01-29 15:36:08 +0800

    first commit

---
+++ eatlas_header_fields_constants.php
@@ -0,0 +1,6 @@
+<?php
+
+// Form fields
+define('EATLAS_HEADER_FIELDS_FIELD_LIBRARIES',  'field_js_libraries');
+define('EATLAS_HEADER_FIELDS_FIELD_CUSTOM_JS',  'field_custom_js');
+define('EATLAS_HEADER_FIELDS_FIELD_CUSTOM_CSS', 'field_custom_css');
